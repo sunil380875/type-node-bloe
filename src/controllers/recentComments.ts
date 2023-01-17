518c8f7a75fae398613d45d5921341ab7f35a548
sunil380875 <sunil380mishra@gmail.com>
2023-01-17 18:14:09 +0530

    new api added

--- src/controllers/recentComments.ts
+++ src/controllers/recentComments.ts
@@ -2,14 +2,29 @@ import express, { Request, Response } from "express";
 import SendResponse from "./../utils/responce";
 
 import { Comment } from "../model/recentCommentsModel";
-export const getAllComment = (req: Request, res: Response) => {};
-
-export const postComment = async (req: Request, res: Response) => {
+export const getAllComment = async (req: Request, res: Response) => {
   try {
-    const comments = await Comment.create(req.body);
+    const comment = await Comment.find();
+    SendResponse(res, 200, comment);
   } catch (err) {
     console.log(err);
   }
 };
 
-export const deleteComment = () => {};
+export const postComment = async (req: Request, res: Response) => {
+  try {
+    const comments = await Comment.create(req.body);
+    SendResponse(res, 201, comments);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const deleteComment = async (req: Request, res: Response) => {
+  try {
+    await Comment.findByIdAndDelete(req.params.id);
+    SendResponse(res, 204, "");
+  } catch (err) {
+    console.log(err);
+  }
+};
